85db4e0fe33ca7271fde38f5144c0b4daab60c78
iyo11 <iyo.mail.cn@gmail.com>
2026-01-26 13:14:04 +0800

    + OmniGatedSDPA

--- train/train.py
+++ train/train.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 
     epoch_count = 300
     close_mosaic_count = 45
-    model_name = "yolo11n_C2PSA_DIFF_C3K2WTConv.yaml"
+    model_name = "yolo11n_OmniGatedSDPA.yaml"
     datasets = '/NWPU_VHR.yaml'
     seed = 42
     optimizer = 'SGD'
