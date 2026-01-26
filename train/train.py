eec98e55fdebc0b6be5d47fe9a37341fc29053fe
iyo11 <iyo.mail.cn@gmail.com>
2026-01-26 15:39:05 +0800

    + APCM

--- train/train.py
+++ train/train.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 
     epoch_count = 300
     close_mosaic_count = 45
-    model_name = "yolo11n_ScDConv.yaml"
+    model_name = "yolo11n_APCM.yaml"
     datasets = '/NWPU_VHR.yaml'
     seed = 42
     optimizer = 'SGD'
